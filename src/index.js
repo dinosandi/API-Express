d8e854d98179de9bee7e6567a89ae5a38c600ca5
Dino Sandi <dinosandi05@gmail.com>
2025-12-18 14:29:53 +0700

    SetUp-Prisma-PostgreSql

--- src/index.js
+++ src/index.js
@@ -1,16 +1,31 @@
-const express = require('express');
-const dotenv = require('dotenv');
+const express = require("express");
+const dotenv = require("dotenv");
+const { PrismaClient } = require("@prisma/client");
+
+const { PrismaPg } = require("@prisma/adapter-pg");
+const { Pool } = require("pg");
+
+dotenv.config();
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
+
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-dotenv.config({});
+app.get("/api", (req, res) => {
+  res.send("selamat datang di API!");
+});
 
-const PORT = process.env.PORT; // untuk mendapatkan port dari variabel lingkungan
-
-app.get('/api', (req, res) => {
-    res.send("selmat datang diAPI!");
-    
-})
+app.get("/products", async (req, res) => {
+  const products = await prisma.product.findMany();
+  res.json(products);
+});
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-})
+  console.log(`Server is running on port ${PORT}`);
+});
