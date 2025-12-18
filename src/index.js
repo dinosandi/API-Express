69e82e02baeaa91c81aabc09af1b7a5dd9be7447
Dino Sandi <dinosandi05@gmail.com>
2025-12-18 21:30:51 +0700

    API-CRUD

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const { PrismaClient } = require("@prisma/client");
 
 const { PrismaPg } = require("@prisma/adapter-pg");
 const { Pool } = require("pg");
+const req = require("express/lib/request");
+const res = require("express/lib/response");
 
 dotenv.config();
 
@@ -17,15 +19,76 @@ const prisma = new PrismaClient({ adapter });
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
 app.get("/api", (req, res) => {
   res.send("selamat datang di API!");
 });
 
 app.get("/products", async (req, res) => {
   const products = await prisma.product.findMany();
-  res.json(products);
+  res.send({
+    data:{
+        products
+    }
+  });
 });
+
+app.get('/users', async (req, res) => {
+    const users = await prisma.user.findMany();
+    res.send(users);
+});
+
+app.post('/products', async (req, res) => {
+    const newProdactData = req.body;
+    const product = await prisma.product.create({
+        data: {
+            name: newProdactData.name,
+            imageUrl: newProdactData.imageUrl,
+            description: newProdactData.description,
+            price: newProdactData.price
+        }
+    })
+    res.status(200).send({
+        data: product,
+        message:"Successfully added new product"
+    });
+});
+
+app.delete('/products/:id', async (req,res) => {
+    const productId = req.params.id;
+    await prisma.product.delete ({
+        where: {
+            id: productId
+        }
+    });
+    res.status(200).send ({
+        message: "Successfully deleted product"
+    });
+    console.log("Successfully deleted product");
+});
+
+app.put('/products/:id', async (req,res) => {
+    const editProductId = req.params.id;
+    const productData = req.body;
+    const updateProduct = await prisma.product.update ({
+        where: {
+            id: editProductId
+        },
+        data: {
+            name: productData.name,
+            imageUrl: productData.imageUrl,
+            description: productData.description,
+            price: productData.price
+        }
+    })
+    res.status(200).send ({
+        data: updateProduct,
+        message: "Successfully updated product"
+    })
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  console.log(Object.keys(prisma));
+
 });
